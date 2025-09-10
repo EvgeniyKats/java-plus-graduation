4b2db5ebdd201024ce444e9fd6dff8612fd3f8bf
Evgeniy Kats <eSunr1se@yandex.ru>
2025-09-10 18:31:55 +0500

    add microservice architecture
    * added config-server
    * added discovery-server
    * added gateway
    * updated main-service, stats-server (register in discovery-server, getting config from config-server)
    * stats-client now using Feign

---
+++ core/main-service/src/main/java/ru/practicum/main/service/MainServiceApplication.java
@@ -0,0 +1,15 @@
+package ru.practicum.main.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"ru.practicum.main.service", "client"})
+@EnableFeignClients(basePackages = "client")
+public class MainServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainServiceApplication.class, args);
+    }
+}
--- stats/stats-client/src/main/java/client/StatsClient.java
+++ stats/stats-client/src/main/java/client/StatsClient.java
@@ -1,23 +1,32 @@
 package client;
 
-import exception.NullBodyException;
-import exception.RequestException;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.practicum.stats.dto.EndpointHitDto;
 import ru.practicum.stats.dto.ViewStatsDto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@FeignClient(name = "stats-server")
 public interface StatsClient {
-    /**
-     * @param hitDto             - тело запроса
-     * @throws RequestException  - генерируется, если статус ответа 4xx/5xx
-     */
-    void createHit(EndpointHitDto hitDto) throws RequestException;
 
-    /**
-     * @param statParam       - данные для формирования запроса
-     * @throws RequestException  - генерируется, если статус ответа 4xx/5xx
-     * @throws NullBodyException - генерируется, если тело ответа пустое
-     */
-    List<ViewStatsDto> getStat(StatParam statParam) throws RequestException, NullBodyException;
+    @GetMapping("/stats")
+    List<ViewStatsDto> getStats(
+            @RequestParam(name = "start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam(name = "end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(name = "uris", required = false) List<String> uris,
+            @RequestParam(name = "unique", defaultValue = "false") Boolean unique
+    );
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    void hitStat(@Valid @RequestBody EndpointHitDto hitDto);
 }
