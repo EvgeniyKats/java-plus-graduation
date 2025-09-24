a43b4f26ba684c2ae2a28e4e8bda3c81192f3634
Evgeniy Kats <eSunr1se@yandex.ru>
2025-09-24 15:42:31 +0500

    add aggregator

---
+++ stats/aggregator/src/main/java/ru/practicum/aggregator/util/calculator/SimilarityCalculatorAvro.java
@@ -0,0 +1,43 @@
+package ru.practicum.aggregator.util.calculator;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.aggregator.storage.SimilarityData;
+import ru.practicum.ewm.stats.avro.EventSimilarityAvro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class SimilarityCalculatorAvro implements SimilarityCalculator<EventSimilarityAvro> {
+    /**
+     * Косинусное сходство между мероприятиями A и B:
+     * similarity(A,B) = сумма минимальных весов A и B / (sqrt(сумма A) * sqrt(сумма B)
+     */
+
+    @Override
+    public List<EventSimilarityAvro> calculate(CalculatorParam param) {
+        if (param.events().isEmpty()) {
+            return List.of(); // нет мероприятий для получения схожести
+        }
+
+        List<EventSimilarityAvro> ans = new ArrayList<>();
+
+        for (SimilarityData data : param.events()) {
+            double sqrtA = Math.sqrt(data.aSumWeight());
+            double sqrtB = Math.sqrt(data.bSumWeight());
+            double minOfAB = data.minWeight();
+
+            double score = minOfAB / (sqrtA * sqrtB);
+
+            EventSimilarityAvro similarity = EventSimilarityAvro.newBuilder()
+                    .setEventA(Math.min(data.aId(), data.bId()))
+                    .setEventB(Math.max(data.aId(), data.bId()))
+                    .setScore(score)
+                    .setTimestamp(param.timestamp())
+                    .build();
+            ans.add(similarity);
+        }
+
+        return ans;
+    }
+}
